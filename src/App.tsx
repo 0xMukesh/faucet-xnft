e1930108103981c019f2db357dc37e8bf3483ae1
Mukesh <hey.mukesh272921@gmail.com>
2022-10-06 13:20:26 +0530

    ✨ feat: lil react hooks stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import * as React from 'react';
 import ReactXnft, { Button, Text, View, usePublicKey } from 'react-xnft';
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
 
@@ -9,20 +9,40 @@ ReactXnft.events.on('connect', () => {
 export const App = () => {
   const publicKey = usePublicKey();
   const address = publicKey.toString();
-  const [fundsSent, setFundsSent] = useState(false);
+  const [transactionLink, setTransactionLink] = React.useState<string | null>(
+    ''
+  );
+  const [balance, setBalance] = React.useState<number | null>(null);
+  const [error, setError] = React.useState<string | null>(null);
+  // @ts-ignore
+  const rpcUrl = window.xnft.solana.connection._rpcEndpoint;
+  const connection = new Connection(rpcUrl);
+  const wallet = new PublicKey(address);
+
+  React.useEffect(() => {
+    if (publicKey) {
+      (async () => {
+        const balance = await connection.getBalance(wallet);
+        setBalance(balance / LAMPORTS_PER_SOL);
+      })();
+    }
+  }, [publicKey]);
 
   const requestAirDrop = async () => {
-    // @ts-ignore
-    const rpcUrl = window.xnft.solana.connection._rpcEndpoint;
-    console.log('rpcUrl', rpcUrl);
-    const connection = new Connection(rpcUrl);
-    const wallet = new PublicKey(address);
-    const signature = await connection.requestAirdrop(
-      wallet,
-      Number(LAMPORTS_PER_SOL)
-    );
+    try {
+      const signature = await connection.requestAirdrop(
+        wallet,
+        Number(LAMPORTS_PER_SOL)
+      );
 
-    console.log('signature: ', signature);
+      console.log('signature: ', signature);
+      setTransactionLink(
+        `https://explorer.solana.com/tx/${signature}?cluster=devnet`
+      );
+    } catch (e) {
+      setError(e.message);
+      console.log(e);
+    }
   };
 
   return (
